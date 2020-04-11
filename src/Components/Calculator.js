55762aab635e1e0d5ee36d400c2cd1f34257639b
Jorge Pasco <jorgepasco@Jorges-MacBook-Pro.local>
2020-04-11 18:30:08 -0500

    Finish calculator layout

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,31 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import Button from "./Button.js";
 import "./Calculator.css";
 
-const Calculator = () => {
-  return (
-    <div className="Calculator">
-      <Button type="secOperator" content="AC" />
-      <Button type="secOperator" content="+/-" />
-      <Button type="secOperator" content="%" />
-      <Button type="primOperator" content="÷" />
-      <Button type="number" content="7" />
-      <Button type="number" content="8" />
-      <Button type="number" content="9" />
-      <Button type="primOperator" content="x" />
-      <Button type="number" content="4" />
-      <Button type="number" content="5" />
-      <Button type="number" content="6" />
-      <Button type="primOperator" content="-" />
-      <Button type="number" content="1" />
-      <Button type="number" content="2" />
-      <Button type="number" content="3" />
-      <Button type="primOperator" content="+" />
-      <Button type="zeroNumber" content="0" />
-      <Button type="number" content="." />
-      <Button type="primOperator" content="=" />
-    </div>
-  );
-};
+export default class Calculator extends Component {
+  constructor() {
+    super();
+    this.state = {
+      display: 0,
+    };
+  }
 
-export default Calculator;
+  render() {
+    return (
+      <div className="Calculator">
+        <div class="monitor">{this.state.display}</div>
+        <Button type="secOperator" content="AC" />
+        <Button type="secOperator" content="+/-" />
+        <Button type="secOperator" content="%" />
+        <Button type="primOperator" content="÷" />
+        <Button type="number" content="7" />
+        <Button type="number" content="8" />
+        <Button type="number" content="9" />
+        <Button type="primOperator" content="x" />
+        <Button type="number" content="4" />
+        <Button type="number" content="5" />
+        <Button type="number" content="6" />
+        <Button type="primOperator" content="-" />
+        <Button type="number" content="1" />
+        <Button type="number" content="2" />
+        <Button type="number" content="3" />
+        <Button type="primOperator" content="+" />
+        <Button type="zeroNumber" content="0" />
+        <Button type="number" content="." />
+        <Button type="primOperator" content="=" />
+      </div>
+    );
+  }
+}
