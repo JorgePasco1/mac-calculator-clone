cc2b5593200c9780d03206b966534d31e7ae0a68
Jorge Pasco <jorgepasco@Jorges-MacBook-Pro.local>
2020-04-15 00:42:55 -0500

    finish primary operations

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -2,17 +2,19 @@ import React, { Component } from "react";
 import Button from "./Button.js";
 import "./Calculator.css";
 
+const initialState = {
+  accumulator: 0,
+  currentInput: "",
+  partialAcc: 0,
+  currentOperation: "none",
+  currentState: "input",
+  fontSize: 3.5,
+}
+
 export default class Calculator extends Component {
   constructor() {
     super();
-    this.state = {
-      accumulator: 0,
-      currentInput: "",
-      partialAcc: 0,
-      currentOperation: "none",
-      currentState: "input",
-      fontSize: 3.5,
-    };
+    this.state = initialState;
   }
 
   handleNumberClick = (character) => {
@@ -52,32 +54,37 @@ export default class Calculator extends Component {
         ...prevState,
         partialAcc: partialResult,
         currentInput: partialResult,
+        currentState: "newResult",
+      }));
+    } else if (currentOp === "addition" || currentOp === "substraction"){
+      this.setState((prevState) => ({
+        ...prevState,
+        accumulator: prevState.accumulator + +prevState.currentInput,
+        currentInput: prevState.accumulator + +prevState.currentInput,
+        currentState: "newResult",
       }));
     }
   };
 
   handleEquals = () => {
     this.doCalculation();
-    this.setState((prevState) => ({
-      ...prevState,
-      accumulator: prevState.accumulator + prevState.partialAcc,
-      currentInput: prevState.accumulator + prevState.partialAcc,
-      accumulator: 0,
-      partialAcc: 0,
-      currentOperation: "none",
-    }));
+    if (
+      this.state.currentOperation === "multiplication" ||
+      this.state.currentOperation === "division"
+    ) {
+      this.setState((prevState) => ({
+        ...prevState,
+        accumulator: prevState.accumulator + prevState.partialAcc,
+        currentInput: prevState.accumulator + prevState.partialAcc,
+        partialAcc: 0,
+        currentOperation: "none",
+      }));
+    }
   };
 
   clearInput = (name) => {
     if (name === "AC") {
-      this.setState((prevState) => ({
-        accumulator: 0,
-        partialAcc: 0,
-        lastOperation: "",
-        currentInput: "",
-        fontSize: 3.5,
-        currentState: "input",
-      }));
+      this.setState((initialState));
     } else {
       this.setState((prevState) => ({
         ...prevState,
@@ -93,6 +100,18 @@ export default class Calculator extends Component {
         ...prevState,
         partialAcc: +prevState.currentInput,
         currentOperation: operator === "x" ? "multiplication" : "division",
+        currentState: "newInput",
+      }));
+    } else {
+      if (
+        this.state.currentOperation === "multiplication" ||
+        this.state.currentOperation === "division"
+      ) {
+        this.handleEquals();
+      }
+      this.setState((prevState) => ({
+        ...prevState,
+        currentOperation: operator === "+" ? "addition" : "substraction",
         currentState: "newInput",
       }));
     }
